1d68640008a2263e83088738134bf283ad38e397
Ziyun Lu <zl2961@columbia.edu>
2021-04-06 06:39:23 +0000

    Debug import command

--- tracker/management/commands/import_squirrel_data.py
+++ tracker/management/commands/import_squirrel_data.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from SquirrelFinder.models import SquirrelTracker
+from tracker.models import SquirrelTracker
 import csv
 import os
 import datetime
