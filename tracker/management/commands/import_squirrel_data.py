f58e7b0188331300782cb82874e8f3a4c2ed8a8a
Ziyun Lu <zl2961@columbia.edu>
2021-04-09 14:52:12 +0000

    Debug the import command

--- tracker/management/commands/import_squirrel_data.py
+++ tracker/management/commands/import_squirrel_data.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = 'Import squirrel data from csv file'
 
     def add_arguments(self,parser):
-        parser.add_argument('path', nargs='+',type = str)
+        parser.add_argument('path', type = str)
         
     def handle(self, *args, **kwargs):
 
@@ -20,35 +20,32 @@ class Command(BaseCommand):
             else:
                 return None
 
-
-        for path in kwargs['path']:
-            self.stdout.write(self.style.SUCCESS('Reading:{}'.format(path)))
-            with open(path) as csv_file:
+            with open(path, encoding = 'utf-8') as csv_file:
                 csv_reader = csv.DictReader(csv_file)
                 data = list(csv_reader)
-                for row in data:
-                    ST = SquirrelTracker()
-                    ST.X = float(row['x'])
-                    ST.Y = float(row['y'])
-                    ST.Unique_Squirrel_ID = row['unique_squirrel_id']
-                    ST.Shift = row['shift']
-                    ST.Date = datetime.datetime.strptime(row['date'],'%m%d%Y')
-                    ST.Age = row['age']
-                    ST.Primary_Fur_Color = row['primary_fur_color']
-                    ST.Location = row['location']
-                    ST.Specific_Location = row['specific_location']
-                    ST.Running = convert_boo(row['running'])
-                    ST.Chasing = convert_boo(row['chasing'])
-                    ST.Climbing = convert_boo(row['climbing'])
-                    ST.Eating = convert_boo(row['eating'])
-                    ST.Foraging = convert_boo(row['foraging'])
-                    ST.Other_Activities = row['other_activities']
-                    ST.Kuks = convert_boo(row['kuks'])
-                    ST.Quaas = convert_boo(row['quaas'])
-                    ST.Moans = convert_boo(row['moans'])
-                    ST.Tail_Flags = convert_boo(row['tail_flags'])
-                    ST.Tail_Twitches = convert_boo(row['tail_twitches'])
-                    ST.Approaches = convert_boo(row['approaches'])
-                    ST.Indifferent = convert_boo(row['indifferent'])
-                    ST.Runs_From =convert_boo(row['runs_from'])
-                    ST.save()
+            for row in data:
+                ST = SquirrelTracker()
+                ST.X = row['X']
+                ST.Y = row['Y']
+                ST.Unique_Squirrel_ID = row['Unique Squirrel ID']
+                ST.Shift = row['Shift']
+                ST.Date = datetime.datetime.strptime(row['Date'],'%m%d%Y')
+                ST.Age = row['Age']
+                ST.Primary_Fur_Color = row['Primary Fur Color']
+                ST.Location = row['Location']
+                ST.Specific_Location = row['Specific Location']
+                ST.Running = convert_boo(row['Running'])
+                ST.Chasing = convert_boo(row['Chasing'])
+                ST.Climbing = convert_boo(row['Climbing'])
+                ST.Eating = convert_boo(row['Eating'])
+                ST.Foraging = convert_boo(row['Foraging'])
+                ST.Other_Activities = row['Other Activities']
+                ST.Kuks = convert_boo(row['Kuks'])
+                ST.Quaas = convert_boo(row['Quaas'])
+                ST.Moans = convert_boo(row['Moans'])
+                ST.Tail_Flags = convert_boo(row['Tail flags'])
+                ST.Tail_Twitches = convert_boo(row['Tail twitches'])
+                ST.Approaches = convert_boo(row['Approaches'])
+                ST.Indifferent = convert_boo(row['Indifferent'])
+                ST.Runs_From =convert_boo(row['Runs from'])
+                ST.save()
