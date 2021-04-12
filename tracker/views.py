2fb7410cb959fa69f4e93d39f906f70051ef7b99
Ziyun Lu <zl2961@columbia.edu>
2021-04-12 12:45:49 +0000

    Add a index page

--- tracker/views.py
+++ tracker/views.py
@@ -6,6 +6,13 @@ from .forms import Form, CreateForm
 def index(request):
     return render(request, 'tracker/index.html', {})
 
+def index(request):
+    squirrels = SquirrelTracker.objects.all()
+    context = {
+        'squirrels': squirrels
+    }
+    return render(request, 'tracker/index.html', context)
+
 def show_map(request):
     sightings = SquirrelTracker.objects.all()[:100]
     context = {
