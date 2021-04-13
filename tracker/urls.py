e4a71a3bce868c1775b05363eba38059334f7fb8
Aeri Liu <81467827+al4109@users.noreply.github.com>
2021-04-13 07:02:59 -0400

    Update urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.index),
+        path('map/',views.map),
+        path('sightings/stats',views.stats_sighting),
+        path('sightings/',views.all_sighting),
+        path('sightings/add',views.create_sighting,name='create_sighting'),
+        path('sightings/<unique_squirrel_id>',views.update_sighting,name='edit_sighting'),
 ]
