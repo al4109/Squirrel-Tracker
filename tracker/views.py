feca3d5bc3165c6fd7d7a11a49e47f449d72805b
Aeri Liu <81467827+al4109@users.noreply.github.com>
2021-04-13 07:39:09 -0400

    Update views.py

--- tracker/views.py
+++ tracker/views.py
@@ -20,3 +20,51 @@ def show_map(request):
     }
     return render(request, 'tracker/map.html', context)
 
+def all_sighting(request):
+    
+def stats_sighting(request):
+    statdict = [
+            SquirrelTracker.objects.filter(Age ='Adult').aggregate(Adult_Squirrels = Count('unique_squirrel_id')),
+            SquirrelTracker.objects.filter(Primary Fur Color ='Cinnamon').aggregate(Cinnamon_Color_Squirrels = Count('unique_squirrel_id')),
+            SquirrelTracker.objects.filter(Location ='Above Ground').aggregate(Found_Above_Ground_Squirrels = Count('unique_squirrel_id')),
+            SquirrelTracker.objects.filter(Shift ='AM').aggregate(Sightings_Occured_Morning = Count('unique_squirrel_id')),
+            SquirrelTracker.objects.filter(Date ='10062018').aggregate(Sightings_Occured_October6th_2018 = Count('unique_squirrel_id')),
+            ]
+    stat_list=[]
+    for stat in statdict:
+        stat_list.append([list(stat.keys())[0],list(stat.values())[0]])
+
+    context = {'stats':sighting_list,}
+    return render(request, 'tracker/stats.html', context)
+    
+def create_sighting(request):
+    if request.method =='POST':
+        #check data with form
+        form = SquirrelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/traker/sightings/')
+    else:
+        #build empty form
+        form = SquirrelForm()
+    context = {
+            'form':form,
+            }
+    return render(request,'tracker/create.html',context)
+    
+def edit_sighting(request, unique_squirrel_id):
+    squirrel= SquirrelTracker.objects.get( pk =Unique_Squirrel_ID)
+    if request.method =='POST':
+        #check data with form
+        form = SquirrelForm(request.POST, instance= squirrel)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/traker/sightings/')
+    else:
+        form = SquirrelForm(instance = squirrel)
+        context = {
+            'form':form,
+            'squirrel':squirrel,
+            }
+    return render(request,'tracker/edit.html',context)
+    
